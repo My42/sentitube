b8843f877cee91a4697a37a3d7e3637186f6f5aa
Mesquita Vincent <vmqt.pro@gmail.com>
2025-02-24 00:42:54 +0100

    feat: analyze comments

--- api/main.py
+++ api/main.py
@@ -4,6 +4,7 @@ from injector import Injector
 from models.youtube_comment import YoutubeComment
 from models.youtube_video import YoutubeVideo
 from repositories.youtube_repository import YoutubeRepository
+from services.sentiment_analyzer_service import SentimentAnalyserService
 
 load_dotenv()
 
@@ -16,15 +17,16 @@ injector = Injector()
 @app.get("/analyze/{video_id}")
 async def analyze_sentiments_of_video(video_id: str):
     yt = injector.get(YoutubeRepository)
+    sentiment_analyzer = injector.get(SentimentAnalyserService)
 
     # TODO: check if data in DB
 
     yt_video = await yt.get_video_by_id(video_id)
     yt_comments = await yt.get_comments_by_video_id(video_id, count=10)
+    comments = sentiment_analyzer.analyze_comments(yt_video, yt_comments)
     # TODO: Save video & comments data in DB
-    # TODO: Analyze sentiments
 
-    return {"message": "Done !"}
+    return comments
 
 
 @app.get("/videos/{id}")
--- api/services/sentiment_analyzer_service.py
+++ api/services/sentiment_analyzer_service.py
@@ -1,3 +1,62 @@
+from langchain_core.output_parsers import JsonOutputParser
+from langchain_core.prompts import PromptTemplate
+from langchain_openai import ChatOpenAI
+
+from models.youtube_comment import YoutubeComment
+from models.youtube_video import YoutubeVideo
+
+
 class SentimentAnalyserService:
-    def analyze_comments(video: dict, comments: list):
-        pass
+    def __init__(self):
+        self.__llm = ChatOpenAI(model="gpt-4o")
+
+    def analyze_comments(
+        self, yt_video: YoutubeVideo, yt_comments: list[YoutubeComment]
+    ):
+        prompt = """
+        You are an AI specialized in sentiment analysis of YouTube comments. Your goal is to analyze the sentiment of each comment in the context of the video's title. The sentiment score must be a float between -1 and 1, where:
+
+        * -1 represents a highly negative sentiment
+        * 0 represents a neutral sentiment
+        * 1 represents a highly positive sentiment
+        
+        If a comment is irrelevant to the video's title, assign null as the score.
+        You will receive the video title as a string and the comments as a list.
+
+        Instructions:
+        Contextual Analysis: Evaluate each comment based on the meaning and sentiment in relation to the video title.
+        Sentiment Scoring: Assign a sentiment score between -1 and 1. Use intermediate values (e.g., -0.5, 0.3) for nuanced sentiment.
+        Relevance Check: If a comment is unrelated to the video’s topic, set its sentiment score to null.
+        Explain Your Score: Provide a short justification for each score based on the comment's content and its relation to the title. Don't need to rewrite the comment in the justification and starts it with "It"
+        
+        The video title is: {video_title}
+        The comments are: {comments}
+
+        Return a JSON object for each comment too prevent error when displaying it. beetween each "json" block add "--"
+        """
+
+        parser = JsonOutputParser()
+        prompt_template = PromptTemplate.from_template(
+            prompt,
+        )
+
+        chain = prompt_template | self.__llm
+        response = chain.invoke(
+            {
+                "video_title": yt_video.title,
+                "comments": [
+                    {"id": comment.id, "text": comment.text} for comment in yt_comments
+                ],
+            }
+        )
+
+        comment_group_by_id = {
+            yt_comment.id: yt_comment.text for yt_comment in yt_comments
+        }
+        scores = [
+            {**score, "text": comment_group_by_id[score["id"]]}
+            for json in response.content.split("--")
+            if (score := parser.parse(json))
+        ]
+
+        return scores
